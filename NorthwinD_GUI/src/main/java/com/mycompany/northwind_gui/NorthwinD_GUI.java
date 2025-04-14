6d48eccbf00e05985792b5b43ffb60007deeb8e6
marcelstoltz00 <marcelstoltz00@gmail.com>
2025-04-14 12:48:14 +0200

    'added netbeans to github'

---
+++ NorthwinD_GUI/src/main/java/com/mycompany/northwind_gui/NorthwinD_GUI.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.northwind_gui;
+
+/**
+ *
+ * @author marcelstoltz
+ */
+public class NorthwinD_GUI {
+
+    public static void main(String[] args) {
+                java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+             new Main_Form().setVisible(true);
+               
+            }
+        });
+
+    }
+}
